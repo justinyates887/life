b8937f7e22241c5669042e7103fa5ddce212e5c2
Justin Yates <justinyates887@gmail.com>
2024-05-23 17:59:42 -0700

    Refresh

---
+++ Life.API/Life.API/Objects/Budget/MonthlyBudgetItem.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class MonthlyBudgetItem
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    [BsonElement("name")]
+    public string? Name { get; set; }
+
+    [BsonElement("category")]
+    public string? Category { get; set; }
+
+    [BsonElement("amount")]
+    public decimal? Amount { get; set; }
+
+    [BsonElement("date")]
+    public DateTime? Date { get; set; }
+
+    [BsonElement("automated")]
+    public bool? Automated { get; set; }
+
+    [BsonElement("variable")]
+    public bool? Variable { get; set; }
+
+    [BsonElement("isDiscretionary")]
+    public bool? IsDiscretionary { get; set; }
+}
